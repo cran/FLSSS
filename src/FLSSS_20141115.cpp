bf3b7d177451aec8b50e165193a0f9284b946b95
Charlie Wusuo Liu <liuwusuo@gmail.com>
2014-11-16 00:00:00 +0000

    version 3.1

--- src/FLSSS_20141115.cpp
+++ src/FLSSS_20141115.cpp
@@ -226,65 +226,65 @@ double target,sumLBI,sumUBI;
 std::vector<NumericVector::iterator>LBI,UBI,UBILeftReserve;
 };
 
-unsigned update(PAT&P){
-if(P.s==P.send)return 0;
-P.target=P.target+*(P.s);
-if(P.position==0)
+unsigned update(std::vector<PAT>::iterator P){
+if(P->s==P->send)return 0;
+P->target=P->target+*(P->s);
+if(P->position==0)
 {
-    ++P.s;
-    P.target=P.target-*(P.s);
-    std::vector<NumericVector::iterator>::iterator bound=P.LBI.begin();
-    NumericVector::iterator iter=P.s+1, tmp=*bound;
-    for(;bound!=P.LBI.end();bound++,iter++)
+    ++P->s;
+    P->target=P->target-*(P->s);
+    std::vector<NumericVector::iterator>::iterator bound=P->LBI.begin();
+    NumericVector::iterator iter=P->s+1, tmp=*bound;
+    for(;bound!=P->LBI.end();bound++,iter++)
     {
         if(iter>*bound)*bound=iter;
         else 
         { 
-            P.sumLBI=P.sumLBI-*tmp+*(iter-1); 
+            P->sumLBI=P->sumLBI-*tmp+*(iter-1); 
             break;
         }
     }
 }
-else if(P.position==P.UBI.size())
+else if(P->position==P->UBI.size())
 {
-    --P.s;
-    P.target=P.target-*(P.s);
-    std::vector<NumericVector::iterator>::iterator bound=P.UBI.end()-1;
-    NumericVector::iterator iter=P.s-1, tmp=*bound;
-    for(;bound>=P.UBI.begin();bound--,iter--)
+    --P->s;
+    P->target=P->target-*(P->s);
+    std::vector<NumericVector::iterator>::iterator bound=P->UBI.end()-1;
+    NumericVector::iterator iter=P->s-1, tmp=*bound;
+    for(;bound>=P->UBI.begin();bound--,iter--)
     {
         if(iter<*bound)*bound=iter;
         else 
         { 
-            P.sumUBI=P.sumUBI-*tmp+*(iter+1);
+            P->sumUBI=P->sumUBI-*tmp+*(iter+1);
             break;
         }
     }
 }
 else
 {
-    ++P.s;
-    P.target=P.target-*(P.s);
-    std::vector<NumericVector::iterator>::iterator Lp_right=P.LBI.begin()+P.position, Up_left=P.UBI.begin()+P.position-1;
-    std::vector<NumericVector::iterator>::iterator ULRi=P.UBILeftReserve.end()-1;
-    NumericVector::iterator iter=P.s-1, tmp;
+    ++P->s;
+    P->target=P->target-*(P->s);
+    std::vector<NumericVector::iterator>::iterator Lp_right=P->LBI.begin()+P->position, Up_left=P->UBI.begin()+P->position-1;
+    std::vector<NumericVector::iterator>::iterator ULRi=P->UBILeftReserve.end()-1;
+    NumericVector::iterator iter=P->s-1, tmp;
     std::vector<NumericVector::iterator>::iterator bound=Up_left;
-    double sumUBIright=P.sumUBI-itersum(P.UBI.begin(),Up_left+1);
-    for(;bound>=P.UBI.begin();bound--,iter--,ULRi--)
+    double sumUBIright=P->sumUBI-itersum(P->UBI.begin(),Up_left+1);
+    for(;bound>=P->UBI.begin();bound--,iter--,ULRi--)
     {
         if(iter<=*ULRi)*bound=iter;
         else break;
     }
-    P.sumUBI=itersum(P.UBI.begin(),Up_left+1)+sumUBIright;
-    iter=P.s+1;
+    P->sumUBI=itersum(P->UBI.begin(),Up_left+1)+sumUBIright;
+    iter=P->s+1;
     bound=Lp_right;
     tmp=*bound;
-    for(;bound!=P.LBI.end();bound++,iter++)
+    for(;bound!=P->LBI.end();bound++,iter++)
     {
         if(iter>*bound)*bound=iter;
         else 
         { 
-            P.sumLBI=P.sumLBI-*tmp+*(iter-1);
+            P->sumLBI=P->sumLBI-*tmp+*(iter-1);
             break; 
         }
     }
@@ -292,72 +292,74 @@ else
 return 1;
 }
 
-unsigned giveBirth(PAT&child,NumericVector&v,double&ME,std::vector<std::vector<double> >&M){
-unsigned len=child.UBI.size();
-unsigned boo=FindBoundsCpp9_1_1(len,v,child.target,ME,child.LBI,child.sumLBI,child.UBI,child.sumUBI,M);
+unsigned giveBirth(std::vector<PAT>::iterator&child,NumericVector&v,
+double&ME,std::vector<std::vector<double> >&M){
+unsigned len=child->UBI.size();
+unsigned boo=FindBoundsCpp9_1_1(len,v,child->target,ME,child->LBI,child->sumLBI,child->UBI,child->sumUBI,M);
 if(boo==0)return 0;
 if(len==1)return 3;
 if(boo==2)return 2;
-std::vector<NumericVector::iterator>::iterator LBi=child.LBI.begin(), UBi=child.UBI.begin();
+std::vector<NumericVector::iterator>::iterator LBi=child->LBI.begin(), UBi=child->UBI.begin();
 unsigned Min=*UBi-*LBi, temp=Min, i=1; 
-child.position=0;
+child->position=0;
 LBi++;
 UBi++;
 for(;i<len;i++,LBi++,UBi++)
 {
     temp=*UBi-*LBi;
-    if(Min>temp){Min=temp;child.position=i;}
+    if(Min>temp){Min=temp;child->position=i;}
 }
-if(child.position==0)
+if(child->position==0)
 {
-    child.s=*child.LBI.begin();
-    child.send=*child.UBI.begin();
-    child.sumLBI=child.sumLBI-*child.s;
-    child.target=child.target-*child.s;
-    child.sumUBI=child.sumUBI-*child.send;
-    child.LBI.erase(child.LBI.begin());
-    child.UBI.erase(child.UBI.begin());
+    child->s=*child->LBI.begin();
+    child->send=*child->UBI.begin();
+    child->sumLBI=child->sumLBI-*child->s;
+    child->target=child->target-*child->s;
+    child->sumUBI=child->sumUBI-*child->send;
+    child->LBI.erase(child->LBI.begin());
+    child->UBI.erase(child->UBI.begin());
 }
-else if(child.position==len-1)
+else if(child->position==len-1)
 {
-    child.s=*(child.UBI.end()-1);
-    child.send=*(child.LBI.end()-1);
-    child.sumUBI=child.sumUBI-*child.s;
-    child.sumLBI=child.sumLBI-*child.send;
-    child.target=child.target-*child.s;
-    child.LBI.pop_back();
-    child.UBI.pop_back();
+    child->s=*(child->UBI.end()-1);
+    child->send=*(child->LBI.end()-1);
+    child->sumUBI=child->sumUBI-*child->s;
+    child->sumLBI=child->sumLBI-*child->send;
+    child->target=child->target-*child->s;
+    child->LBI.pop_back();
+    child->UBI.pop_back();
 }
 else
 {
     std::vector<NumericVector::iterator>::iterator 
-    Lp=child.LBI.begin()+child.position, Up=child.UBI.begin()+child.position;
-    child.s=*Lp;
-    child.target=child.target-*(child.s);
-    child.send=*Up;
-    child.sumLBI=child.sumLBI-**Lp;
-    child.sumUBI=child.sumUBI-**Up;
-    child.LBI.erase(Lp);
-    child.UBI.erase(Up);
-    child.UBILeftReserve.assign(child.UBI.begin(), child.UBI.begin()+child.position);
-    std::vector<NumericVector::iterator>::iterator Up_left=child.UBI.begin()+child.position-1,
-    ULRi=child.UBILeftReserve.end()-1;
-    NumericVector::iterator iter=child.s-1;
+    Lp=child->LBI.begin()+child->position, Up=child->UBI.begin()+child->position;
+    child->s=*Lp;
+    child->target=child->target-*(child->s);
+    child->send=*Up;
+    child->sumLBI=child->sumLBI-**Lp;
+    child->sumUBI=child->sumUBI-**Up;
+    child->LBI.erase(Lp);
+    child->UBI.erase(Up);
+    child->UBILeftReserve.assign(child->UBI.begin(), child->UBI.begin()+child->position);
+    std::vector<NumericVector::iterator>::iterator Up_left=child->UBI.begin()+child->position-1,
+    ULRi=child->UBILeftReserve.end()-1;
+    NumericVector::iterator iter=child->s-1;
     std::vector<NumericVector::iterator>::iterator bound=Up_left;
-    double sumUBIright=child.sumUBI-itersum(child.UBILeftReserve.begin(),child.UBILeftReserve.end());
-    for(;bound>=child.UBI.begin();bound--,iter--,ULRi--)
+    double sumUBIright=child->sumUBI-itersum(child->UBILeftReserve.begin(),child->UBILeftReserve.end());
+    for(;bound>=child->UBI.begin();bound--,iter--,ULRi--)
     {
         if(iter<=*ULRi)*bound=iter;
         else break;
     }
-    child.sumUBI=itersum(child.UBI.begin(),Up_left+1)+sumUBIright;    
+    child->sumUBI=itersum(child->UBI.begin(),Up_left+1)+sumUBIright;    
 }
 return 1;
 };
 
 void TTT_stack_1_1(unsigned&LEN,NumericVector&v,double&ME,
                 std::vector<std::vector<unsigned> >&result, unsigned&sizeNeeded,
-                std::vector<std::vector<double> >&M, std::vector<PAT>&SK, double&Duration){
+                std::vector<std::vector<double> >&M, std::vector<PAT>&SK, 
+                std::vector<PAT>::iterator&SK_end, double&Duration){
 if(LEN==1)
 {
     double Max=SK.front().target+ME, Min=Max-2*ME;
@@ -373,26 +375,25 @@ if(LEN==1)
     }
     return;
 }
-if(SK.front().UBI.size()==0&&SK.size()<2)return;
+if(SK.front().UBI.size()==0&&SK_end-SK.begin()<2)return;
 unsigned boo;
 std::clock_t timeend=std::clock()+Duration*(double)CLOCKS_PER_SEC;
-PAT child;
 std::vector<unsigned>common(LEN);
 std::vector<unsigned>::iterator xi;
 while(1){
-child=SK.back();
-boo=giveBirth(child,v,ME,M);
+*SK_end=*(SK_end-1);
+boo=giveBirth(SK_end,v,ME,M);
 if(boo==1)
 {
-    SK.push_back(child);
+    ++SK_end;
     continue;
 }
 if(boo==3)
 {
     xi=common.begin();
-    unsigned i=child.LBI.back()-v.begin()+1, end=child.UBI.back()-v.begin()+1, tmp;
+    unsigned i=SK_end->LBI.back()-v.begin()+1, end=SK_end->UBI.back()-v.begin()+1, tmp;
     if(i>end){tmp=i;i=end;end=tmp;}
-    for(std::vector<PAT>::iterator SKi=SK.begin()+1;SKi!=SK.end();++SKi,++xi)*xi=SKi->s-v.begin()+1;
+    for(std::vector<PAT>::iterator SKi=SK.begin()+1;SKi!=SK_end;++SKi,++xi)*xi=SKi->s-v.begin()+1;
     for(;i<=end;i++)
     {
         common.back()=i;
@@ -402,15 +403,15 @@ if(boo==3)
 else if(boo==2)
 {
     xi=common.begin();
-    for(std::vector<PAT>::iterator SKi=SK.begin()+1;SKi!=SK.end();++SKi,++xi)*xi=SKi->s-v.begin()+1;
-    std::vector<NumericVector::iterator>::iterator i=child.UBI.begin();
-    for(;i!=child.UBI.end();++i,++xi)*xi=*i-v.begin()+1;
+    for(std::vector<PAT>::iterator SKi=SK.begin()+1;SKi!=SK_end;++SKi,++xi)*xi=SKi->s-v.begin()+1;
+    std::vector<NumericVector::iterator>::iterator i=SK_end->UBI.begin();
+    for(;i!=SK_end->UBI.end();++i,++xi)*xi=*i-v.begin()+1;
     result.push_back(common);
 }
-while(update(SK.back())==0)
+while(update(SK_end-1)==0)
 {
-    SK.pop_back();
-    if(SK.size()<2)return;
+    --SK_end;
+    if(SK_end-SK.begin()<2)return;
 }
 if((sizeNeeded!=0&&result.size()>=sizeNeeded)||(Duration!=-1&&std::clock()>timeend))break;
 }
@@ -421,8 +422,7 @@ IntegerVector LB, IntegerVector UB, unsigned sizeNeeded, double Duration){
 std::vector<std::vector<unsigned> >result;
 if(sizeNeeded!=0)result.reserve(sizeNeeded+6);
 else result.reserve(1024);
-std::vector<PAT>SK(1);
-SK.reserve(len+6);
+std::vector<PAT>SK(len+6);
 NumericVector::iterator vbegin=v.begin();
 PAT&SKbegin=SK.front();
 SKbegin.LBI.resize(len);
@@ -437,18 +437,19 @@ SKbegin.sumUBI=itersum(SKbegin.UBI.begin(),SKbegin.UBI.end());
 SKbegin.target=target;
 std::vector<std::vector<double> >M(len);
 theMatrix(v,len,M);
-TTT_stack_1_1(len,v,ME,result,sizeNeeded,M,SK,Duration);
+std::vector<PAT>::iterator SK_end=SK.begin()+1;
+TTT_stack_1_1(len,v,ME,result,sizeNeeded,M,SK,SK_end,Duration);
 if(result.size()==0)return List(0);
 List lis(result.size());
 for(unsigned i=0;i<result.size();i++)lis[i]=result[i];
 std::vector<PAT>::iterator pati=SK.begin()+1;
-std::vector<unsigned>I(3*(SK.size()-1));
+std::vector<unsigned>I(3*(SK_end-SK.begin()-1));
 std::vector<double>J(I.size());
 std::vector<std::vector<unsigned> >K(I.size());
 std::vector<unsigned>::iterator Ii=I.begin();
 std::vector<double>::iterator Ji=J.begin();
 std::vector<std::vector<unsigned> >::iterator Ki=K.begin();
-for(;pati!=SK.end();++pati)
+for(;pati!=SK_end;++pati)
 {
     *Ii=pati->position;++Ii;
     *Ii=pati->s-vbegin;++Ii;
@@ -468,13 +469,12 @@ IntegerVector I, NumericVector J, List LK, List LM,unsigned sizeNeeded,double Du
 std::vector<std::vector<unsigned> >K=as<std::vector<std::vector<unsigned> > >(LK);
 std::vector<std::vector<double> >M=as<std::vector<std::vector<double> > >(LM);    
 NumericVector::iterator vbegin=v.begin();
-unsigned SKsize=I.size()/3+1;
-std::vector<PAT>SK(SKsize);
-std::vector<PAT>::iterator pati=SK.begin()+1;
+std::vector<PAT>SK(len+6);
+std::vector<PAT>::iterator SK_end=SK.begin()+I.size()/3+1, pati=SK.begin()+1;
 IntegerVector::iterator Ii=I.begin();
 NumericVector::iterator Ji=J.begin();
 std::vector<std::vector<unsigned> >::iterator Ki=K.begin();
-for(;pati!=SK.end();++pati)
+for(;pati!=SK_end;++pati)
 {
     pati->position=*Ii;++Ii;
     pati->s=vbegin+*Ii;++Ii;
@@ -489,7 +489,7 @@ for(;pati!=SK.end();++pati)
 std::vector<std::vector<unsigned> >result;
 if(sizeNeeded!=0)result.reserve(sizeNeeded+6);
 else result.reserve(1024);
-TTT_stack_1_1(len,v,ME,result,sizeNeeded,M,SK,Duration);
+TTT_stack_1_1(len,v,ME,result,sizeNeeded,M,SK,SK_end,Duration);
 if(result.size()==0)return List(0);
 List lis(result.size());
 for(unsigned i=0;i<result.size();i++)lis[i]=result[i];
@@ -501,13 +501,12 @@ IntegerVector I, NumericVector J, List LK, List LM,unsigned sizeNeeded,double Du
 std::vector<std::vector<unsigned> >K=as<std::vector<std::vector<unsigned> > >(LK);
 std::vector<std::vector<double> >M=as<std::vector<std::vector<double> > >(LM);    
 NumericVector::iterator vbegin=v.begin();
-unsigned SKsize=I.size()/3+1;
-std::vector<PAT>SK(SKsize);
-std::vector<PAT>::iterator pati=SK.begin()+1;
+std::vector<PAT>SK(len+6);
+std::vector<PAT>::iterator SK_end=SK.begin()+I.size()/3+1, pati=SK.begin()+1;
 IntegerVector::iterator Ii=I.begin();
 NumericVector::iterator Ji=J.begin();
 std::vector<std::vector<unsigned> >::iterator Ki=K.begin();
-for(;pati!=SK.end();++pati)
+for(;pati!=SK_end;++pati)
 {
     pati->position=*Ii;++Ii;
     pati->s=vbegin+*Ii;++Ii;
@@ -522,18 +521,18 @@ for(;pati!=SK.end();++pati)
 std::vector<std::vector<unsigned> >result;
 if(sizeNeeded!=0)result.reserve(sizeNeeded+6);
 else result.reserve(1024);
-TTT_stack_1_1(len,v,ME,result,sizeNeeded,M,SK,Duration);
+TTT_stack_1_1(len,v,ME,result,sizeNeeded,M,SK,SK_end,Duration);
 if(result.size()==0)return List(0);
 List lis(result.size());
 for(unsigned i=0;i<result.size();i++)lis[i]=result[i];
 pati=SK.begin()+1;
-std::vector<unsigned>II(3*(SK.size()-1));
+std::vector<unsigned>II(3*(SK_end-SK.begin()-1));
 std::vector<double>JJ(II.size());
 std::vector<std::vector<unsigned> >KK(II.size());
 std::vector<unsigned>::iterator IIi=II.begin();
 std::vector<double>::iterator JJi=JJ.begin();
 std::vector<std::vector<unsigned> >::iterator KKi=KK.begin();
-for(;pati!=SK.end();++pati)
+for(;pati!=SK_end;++pati)
 {
     *IIi=pati->position;++IIi;
     *IIi=pati->s-vbegin;++IIi;
@@ -549,14 +548,13 @@ return List::create(Named("roots",lis),Named("node",List::create(II,JJ,KK,LM)));
 }
 
 List FLSSS_SK(unsigned len, NumericVector v, double target, double ME, 
-IntegerVector LB, IntegerVector UB, unsigned sizeNeeded,double Duration){
+IntegerVector LB, IntegerVector UB, unsigned sizeNeeded, double Duration){
 std::vector<std::vector<unsigned> >result;
 if(sizeNeeded!=0)result.reserve(sizeNeeded+6);
 else result.reserve(1024);
-std::vector<PAT>SK(1);
-SK.reserve(len+6);
+std::vector<PAT>SK(len+6);
 NumericVector::iterator vbegin=v.begin();
-PAT& SKbegin=SK.front();
+PAT&SKbegin=SK.front();
 SKbegin.LBI.resize(len);
 SKbegin.UBI.resize(len);
 for(unsigned i=0;i<len;++i)
@@ -570,14 +568,13 @@ SKbegin.target=target;
 std::vector<std::vector<double> >M(len);
 M.reserve(len);
 theMatrix(v,len,M);
-TTT_stack_1_1(len,v,ME,result,sizeNeeded,M,SK,Duration);
+std::vector<PAT>::iterator SK_end=SK.begin()+1;
+TTT_stack_1_1(len,v,ME,result,sizeNeeded,M,SK,SK_end,Duration);
 if(result.size()==0)return List(0);
 List lis(result.size());
 for(unsigned i=0;i<result.size();i++)lis[i]=result[i];
 return lis;
 }
-
-
 
 // FLSSS_SK_throw
 List FLSSS_SK_throw(unsigned len, NumericVector v, double target, double ME, IntegerVector LB, IntegerVector UB, unsigned sizeNeeded, double Duration);
@@ -669,7 +666,3 @@ BEGIN_RCPP
     return __sexp_result;
 END_RCPP
 }
-
-
-
-
